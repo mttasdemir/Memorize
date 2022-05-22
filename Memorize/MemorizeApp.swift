0970c2a6ac7c1cf8f4f03968ab787fc3726e2c3e
mttasdemir <mt.tasdemir@gmail.com>
2022-05-23 00:44:27 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mustafa Taşdemir on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
